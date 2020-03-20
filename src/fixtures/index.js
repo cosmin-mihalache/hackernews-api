b06572dd676b86f9e12a0af9d34c261eb34d5e3f
Cosmin <co5myn92@yahoo.com>
2020-03-20 22:56:59 +0200

    fixtures

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: 'Cosmin Mihalache',
+  id: 1,
+  time: 1567209822,
+  title: 'Mask: Google Responds',
+  url: 'http://something.com/hackernewspuff',
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined,
+};
